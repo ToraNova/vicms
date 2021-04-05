6ed375c061a7ab0eded7244c8081b41f2c6531b6
Toranova <chia_jason96@live.com>
2021-04-06 01:55:08 +0800

    prep for pypi release

--- vicms/vial.py
+++ vicms/vial.py
@@ -6,18 +6,9 @@ upon every build
 from flask import Blueprint, flash
 from collections import namedtuple
 
-name = 'vicms'
-version = '0.1.4'
-description = 'vial-cms (vicms), a flask mini content management module using sqlalchemy'
-
 AppArch = namedtuple('AppArch', ['bp'])
 
-def make_blueprint(prefix=None):
+def make_blueprint(prefix=None, name='vicms'):
     prefix = prefix if prefix else '/%s'%name
     bp = Blueprint(name, __name__, url_prefix=prefix)
-
-    @bp.route('/about', methods=['GET'])
-    def about():
-        return '%s %s: %s, written by toranova\n' % (name, version, description)
-
     return bp
